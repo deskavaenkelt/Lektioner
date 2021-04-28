485930f52ffe1dfb09d6436310a82cfd801a6b41
Lars Strömberg <stromberg.lars@gmail.com>
2021-04-28 14:57:15 +0200

    Refactor, kommenterar bort kod som inte används

--- 2021-04-28/src/App.js
+++ 2021-04-28/src/App.js
@@ -1,31 +1,39 @@
 import './App.css';
-import Card from "./components/Card";
+// import Card from "./components/Card";
+import CardList from "./components/CardList";
 
 function App({ robots }) {
     return (
         // step1()
-        step2(robots)
+        // step2(robots)
+        step3(robots)
     );
 }
 
 export default App;
 
-function step1() {
-    return (
-        <>
-            <Card id="1" name="John Doe" email="john.doe@gmail.com"/>
-            <Card id="2" name="Jane Doe" email="jane.doe@gmail.com"/>
-            <Card id="3" name="Bengt Doe" email="bengt.doe@gmail.com"/>
-        </>
-    )
-}
+// function step1() {
+//     return (
+//         <>
+//             <Card id="1" name="John Doe" email="john.doe@gmail.com"/>
+//             <Card id="2" name="Jane Doe" email="jane.doe@gmail.com"/>
+//             <Card id="3" name="Bengt Doe" email="bengt.doe@gmail.com"/>
+//         </>
+//     )
+// }
+//
+// function step2(robots) {
+//     return (
+//         <>
+//             <Card id={ robots[0].id } name={ robots[0].name } email={ robots[0].email }/>
+//             <Card id={ robots[1].id } name={ robots[1].name } email={ robots[1].email }/>
+//             <Card id={ robots[2].id } name={ robots[2].name } email={ robots[2].email }/>
+//         </>
+//     )
+// }
 
-function step2(robots) {
+function step3(robots) {
     return (
-        <>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-        </>
+        <CardList robots={ robots }/>
     )
 }
