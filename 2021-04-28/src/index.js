1b5abcc82910107b867aae7ebea198deab3378d5
Lars Strömberg <stromberg.lars@gmail.com>
2021-04-28 14:01:43 +0200

    Importerar robots.js

--- 2021-04-28/src/index.js
+++ 2021-04-28/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'tachyons'
-
+import 'tachyons';
+import { robots } from "./global/robots";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App robots={ robots }/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
