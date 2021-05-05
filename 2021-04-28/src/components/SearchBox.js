6443a141ca0cb6d9174103b09f5304c7b9926b63
Lars Strömberg <stromberg.lars@gmail.com>
2021-05-05 09:05:42 +0200

    cleaned out the folder

--- 2021-04-28/src/components/SearchBox.js
+++
@@ -1,9 +0,0 @@
-export default function SearchBox() {
-    return (
-        <div>
-            <input className='pa3 ba b--green bg-lightest-blue'
-                   type="search"
-                   placeholder='Search robots'/>
-        </div>
-    )
-};
